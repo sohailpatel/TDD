32aff3a886a9172a9bd4ab23d34fbd736ef21938
sohailpatel <sohailpatelt20@gmail.com>
2016-03-04 19:11:58 +0530

    Update person.java

--- TDDproject/src/person.java
+++ TDDproject/src/person.java
@@ -4,7 +4,7 @@ public class person {
 	String name;
 	int age;
 	String gender;
-	public String toString(int id,String name,int age,String gender){
+	public String toString(){
 		return "Id no. "+id+" of name "+name+" having age "+age+" is "+gender;
 		
 	}

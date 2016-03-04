cb66464124c2dedf277fc1755612cf114b82fdea
sohailpatel <sohailpatelt20@gmail.com>
2016-03-04 19:13:42 +0530

    Update PersonTest.java

--- TDDproject/src/PersonTest.java
+++ TDDproject/src/PersonTest.java
@@ -12,9 +12,12 @@ public class PersonTest {
 	}
 	@Test
 	public void test() {
-		assertEquals("Id no. 200 of name qwert having age 90 is male",personString.toString(200,"qwert",90,"male"));
-		assertEquals("Id no. 1 of name abc having age 21 is male",personString.toString(1,"abc",21,"male"));
-		assertEquals("Id no. 4 of name abcdef having age 40 is female",personString.toString(4,"abcdef",40,"female"));
+		personString.id=200;personString.name="qwert";personString.age=90;personString.gender="male";
+		assertEquals("Id no. 200 of name qwert having age 90 is male",personString.toString());
+		personString.id=1;personString.name="abc";personString.age=21;personString.gender="male";
+		assertEquals("Id no. 1 of name abc having age 21 is male",personString.toString());
+		personString.id=4;personString.name="abcdef";personString.age=40;personString.gender="female";
+		assertEquals("Id no. 4 of name abcdef having age 40 is female",personString.toString());
 	}
 
 }
